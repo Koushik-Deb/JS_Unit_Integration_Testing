9f0e40f19e30e1fda82a38e812409defca9ee30e
Koushik-007 <2015-516-802@student.du.ac.bd>
2024-02-24 19:54:37 +0600

    Added integration testing

--- basic-testing-frontend/src/util/numbers.test.js
+++ basic-testing-frontend/src/util/numbers.test.js
@@ -1,43 +1,72 @@
-import { it, expect } from 'vitest';
-import { transformToNumber } from './numbers';
+import { it, expect, describe } from 'vitest';
+import { transformToNumber, cleanNumbers } from './numbers.js';
 
-it('should transform a string to a number', () => {
-    // Arrange
-    const value = '123';
-    const expectedResult = 123;
+describe('transformToNumber()', () => {
+    it('should transform a string to a number', () => {
+        // Arrange
+        const value = '123';
+        const expectedResult = 123;
 
-    // Act
-    const result = transformToNumber(value);
+        // Act
+        const result = transformToNumber(value);
 
-    // Assert
-    // expect(result).toBe(expectedResult);
-    // expect(result).toBeTypeOf('number');
+        // Assert
+        // expect(result).toBe(expectedResult);
+        // expect(result).toBeTypeOf('number');
 
-    expect(result).toBe(expectedResult).toBeTypeOf('number');
+        expect(result).toBe(expectedResult).toBeTypeOf('number');
+    });
+
+    it('should transform a string with a leading zero to a number', () => {
+        // Arrange
+        const value = '0123';
+        const expectedResult = 123;
+
+        // Act
+        const result = transformToNumber(value);
+
+        // Assert
+        expect(result).toBe(expectedResult);
+    });
+
+    it('should return NaN if the value is not a number', () => {
+        // Arrange
+        const value = 'invalid';
+        const value2 = { a: 1, b: 2 }
+
+        // Act
+        const result = transformToNumber(value);
+        const result2 = transformToNumber(value2);
+
+        // Assert
+        expect(result).toBeNaN();
+        expect(result2).toBeNaN();
+    });
 });
 
-it('should transform a string with a leading zero to a number', () => {
-    // Arrange
-    const value = '0123';
-    const expectedResult = 123;
+describe('cleanNumbers()', () => {
+    it('should return an array of numbers', () => {
+        // Arrange
+        const numberValues = ['1', '2', '3'];
+        const expectedResult = [1, 2, 3];
 
-    // Act
-    const result = transformToNumber(value);
+        // Act
+        const result = cleanNumbers(numberValues);
 
-    // Assert
-    expect(result).toBe(expectedResult);
-});
+        // Assert
+        expect(result).toEqual(expectedResult);
+        expect(result[0]).toBeTypeOf('number');
+    });
 
-it('should return NaN if the value is not a number', () => {
-    // Arrange
-    const value = 'invalid';
-    const value2 = { a: 1, b: 2 }
+    it('should throw an error if a value is not a number', () => {
+        // Arrange
+        const numberValues = ['1', 'invalid', '3'];
 
-    // Act
-    const result = transformToNumber(value);
-    const result2 = transformToNumber(value2);
+        // Act
+        const result = () => cleanNumbers(numberValues);
 
-    // Assert
-    expect(result).toBeNaN();
-    expect(result2).toBeNaN();
+        // Assert
+        expect(result).toThrow();
+    });
+
 });
